c8cded6cc8d86a856962426951a1f2c48241e6b2
ThomasF831 <114236206+ThomasF831@users.noreply.github.com>
2022-10-30 22:43:17 +0100

    Commentaires

--- lecture_fichier.ml
+++ lecture_fichier.ml
@@ -1,5 +1,7 @@
+(* lorsqu'on appelle ./Aritha "fichier.exp", t = [|"./Aritha","fichier.exp" *)
 let t = Sys.argv;;
 
+(* extraire_string ouvre fichier.exp.txt et renvoie le texte qu'il contient *)
 let extraire_string () =
   let f = open_in (t.(1)^".txt") in
   let s = input_line f in
